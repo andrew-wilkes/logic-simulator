d8f70b88cb0579d3a438df4a179729e6c6876887
andrew-wilkes <github@urgentclick.com>
2021-05-23 16:24:26 +0900

    Add files

---
+++ src/Main.gd
@@ -0,0 +1,29 @@
+extends VBoxContainer
+
+func _ready():
+	pass
+
+
+
+func _on_not_button_down():
+	pass # Replace with function body.
+
+
+func _on_and_button_down():
+	pass # Replace with function body.
+
+
+func _on_nand_button_down():
+	pass # Replace with function body.
+
+
+func _on_or_button_down():
+	pass # Replace with function body.
+
+
+func _on_nor_button_down():
+	pass # Replace with function body.
+
+
+func _on_xor_button_down():
+	pass # Replace with function body.
