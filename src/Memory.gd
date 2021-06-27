3098cd77dc827aada542f5e89c54207a48e4eb98
andrew-wilkes <github@urgentclick.com>
2021-06-27 17:00:32 +0900

    Add OE and R/W control to memory

--- src/Memory.gd
+++ src/Memory.gd
@@ -13,31 +13,45 @@ func setup():
 	}
 	data.memory.set_mem_size(256)
 	set_mem_size_label_text()
+	value = 0
+	input_levels[1] = false
+	input_levels[2] = false
+	input_levels[3] = false
 
 
 func loaded_from_file():
 	set_mem_size_label_text()
 
 
+func update_output(level: bool, port: int, _reverse: bool):
+		input_levels[port] = level
+
+
 func set_value(v: int, _reverse: bool, _from_pin: bool, port := 0):
-	if port == 1: # Address
+	if port == 0: # Address
 		v %= data.memory.mem_size
 		# If the value is unchanged ignore it
 		if value == v:
 			return
 		value = v
 	else: # Data
+		if input_levels[3]: # /W
+			return
 		if data.memory.width == 8:
 			v %= 0x100
 		if data.memory.bytes[value] == v:
 			return
 		data.memory.bytes[value] = v
-	emit_bus_update()
+	if type == Parts.TYPES.ROM:
+		if not input_levels[1]: # /OE
+			emit_bus_update()
+	else:
+		if not input_levels[2]: # /OE
+			emit_bus_update()
 
 
 func emit_bus_update():
-	var addr = value if value >= 0 else 0
-	emit_signal("bus_changed", self, data.memory.bytes[addr], false)
+	emit_signal("bus_changed", self, data.memory.bytes[value], false)
 
 
 func _on_Button_pressed():
