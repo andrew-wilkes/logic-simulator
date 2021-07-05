371d9f044bd9c4eb79f7279c9f18c64cd65f81c7
andrew-wilkes <github@urgentclick.com>
2021-07-05 13:31:08 +0000

    Fix shift reg

--- src/parts/shiftreg_block.gd
+++ src/parts/shiftreg_block.gd
@@ -8,41 +8,34 @@ func setup():
 		"mode": HEX,
 		"bits": 2,
 	}
-	apply_data()
 
-
-func apply_data():
-	#set_format()
-	update_display_value()
 
 func update_output(_level: bool, _port: int, _r: bool):
 	if input_pins[5].level: # Reset
-		output_enabled = true
-		value = -1 # Make sure it propagates
-		set_value(0, false, false)
+		output_value(0)
 	# Detect not rising edge of CK
 	if not input_pins[4].level or input_pins[4].last_level:
 		return
 	input_pins[4].last_level = input_pins[4].level
-	output_enabled = true
 	if input_pins[3].level: # LD
-		value = -1 # Make sure it propagates
-		set_value(vin, false, false)
+		output_value(vin)
 	else:
 		var v = value
 		if input_pins[2].level: # EN
 			v /= 2 # Shift right
 			if input_pins[1].level: # SI
 				v += msbs[data.bits]
-		set_value(v, false, false)
+		output_value(v)
 
 
 func set_value(v: int, _reverse: bool, _port := 0):
+	vin = v
+
+
+func output_value(v: int):
 	value = v
-	if output_enabled:
-		output_enabled = false
-		update_display_value()
-		emit_signal("bus_changed", self, v, false)
+	update_display_value()
+	emit_signal("bus_changed", self, v, false)
 
 
 func _on_Bits_was_pressed(_b):
