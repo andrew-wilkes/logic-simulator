502d4afccf6f6f7c98f0a04ad13469cf914ad058
andrew-wilkes <github@urgentclick.com>
2021-08-21 13:48:02 +0900

    Add IO parts to grid

--- src/parts/io_block.gd
+++ src/parts/io_block.gd
@@ -5,12 +5,13 @@ class_name IoBlock
 func _ready():
 	._ready()
 	# connect_inner_io_nodes
-	for node in get_children():
-		if node is Control:
-			node.connect("gui_input", self, "on_inner_node_gui_input", [node])
-		for child in node.get_children():
-			if child is Button:
-				child.focus_mode = Control.FOCUS_NONE
+	for col in $Grid.get_children():
+		if col is Control:
+			for node in col.get_children():
+				node.connect("gui_input", self, "on_inner_node_gui_input", [node])
+				for child in node.get_children():
+					if child is Button:
+						child.focus_mode = Control.FOCUS_NONE
 
 
 func on_inner_node_gui_input(event, node):
@@ -19,6 +20,7 @@ func on_inner_node_gui_input(event, node):
 		node.type = node.name # This is the pure name used when loading a circuit
 		if node.get_child(0).name == "V":
 			node.set_value(0, false)
-		remove_child(node)
+		# Unparent the selected node
+		node.get_parent().remove_child(node)
 		emit_signal("part_variant_selected", node, offset)
 		queue_free() # Remove this IoBlock
