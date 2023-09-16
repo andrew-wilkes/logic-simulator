daa3d52896d88a20920cf570bddaf6c8dba16304
andrew-wilkes <github@urgentclick.com>
2023-09-16 19:43:47 +0900

    Add HexSpinBox to Input8

--- src/HexSpinBox.gd
+++ src/HexSpinBox.gd
@@ -3,6 +3,7 @@ extends LineEdit
 signal value_changed(value)
 
 export var num_bits = 8
+export var length = 77
 
 var value = 0
 var count_up = true
@@ -10,17 +11,20 @@ var max_value = 255
 var format = "0x%02X"
 
 func _ready():
-	max_value = 255 if num_bits == 8 else 15
+	max_value = int(pow(2, num_bits)) - 1
+	$HB.rect_size.y = length
 
 
 func _on_Up_pressed():
-	count_up = true
-	change_value()
-	$RepeatTimer.start(0.5)
+	start_repeat(true)
 
 
 func _on_Down_pressed():
-	count_up = false
+	start_repeat(false)
+
+
+func start_repeat(up):
+	count_up = up
 	change_value()
 	$RepeatTimer.start(0.5)
 
@@ -38,7 +42,6 @@ func change_value():
 	elif value > 0:
 		value -= 1
 	set_value(value)
-	emit_signal("value_changed", value)
 
 
 func _on_text_entered(new_text):
@@ -48,14 +51,14 @@ func _on_text_entered(new_text):
 		value = new_text.hex_to_int()
 	set_value(int(clamp(value, 0, max_value)))
 	caret_position = 8
-	emit_signal("value_changed", value)
-
-
-func set_value(v):
-	value = v
-	text = format % [value]
 
 
 func _on_VSlider_value_changed(v):
 	set_value(v * max_value / 100)
-	emit_signal("value_changed", value)
+
+
+func set_value(v, notify = true):
+	value = v
+	text = format % [value]
+	if notify:
+		emit_signal("value_changed", value)
